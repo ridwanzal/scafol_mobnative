2668812eefb3bd34b5f2d7128b7a571686624c88
M. Ridwan Zalbina <zalbinaridwan@gmail.com>
2019-07-26 14:07:11 +0700

    Add Menu tablayout notification

--- ScafolMobile/app/src/main/java/com/example/scafolmobile/activity/ActivityNotif.java
+++ ScafolMobile/app/src/main/java/com/example/scafolmobile/activity/ActivityNotif.java
@@ -1,29 +1,31 @@
 package com.example.scafolmobile.activity;
 
+import android.app.ActionBar;
 import android.os.Bundle;
-import android.view.MenuItem;
+import android.view.View;
+import android.view.Window;
 
-import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
 import com.example.scafolmobile.R;
+import com.example.scafolmobile.tabcompnotif.SectionPagerAdapter;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.android.material.tabs.TabLayout;
 
-public class ActivityNotif extends AppCompatActivity {
+public class ActivityNotif extends AppCompatActivity{
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getWindow().requestFeature(Window.FEATURE_ACTION_BAR);
+        getSupportActionBar().hide();
         setContentView(R.layout.activity_notification);
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        switch (item.getItemId()){
-            case android.R.id.home :
-                finish();
-            default :
-                return super.onOptionsItemSelected(item);
-        }
+        SectionPagerAdapter sectionsPagerAdapter = new SectionPagerAdapter(this, getSupportFragmentManager());
+        ViewPager viewPager = findViewById(R.id.view_pager);
+        viewPager.setAdapter(sectionsPagerAdapter);
+        TabLayout tabs = findViewById(R.id.tabs);
+        tabs.setupWithViewPager(viewPager);
     }
 }

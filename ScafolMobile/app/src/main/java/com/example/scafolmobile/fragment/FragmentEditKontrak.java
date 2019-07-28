0435b4ae83cc5de891041056c98d6c8885c070b3
M. Ridwan Zalbina <zalbinaridwan@gmail.com>
2019-07-28 21:58:37 +0700

    change package name

--- ScafolMobile/app/src/main/java/com/example/scafolmobile/fragment/FragmentEditKontrak.java
+++ ScafolMobile/app/src/main/java/com/example/scafolmobile/fragment/FragmentEditKontrak.java
@@ -1,4 +1,4 @@
-package com.example.scafolmobile.Fragment;
+package com.example.scafolmobile.fragment;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
---
+++ ScafolMobile/app/src/main/java/com/example/scafolmobile/fragment/FragmentProgress.java
@@ -0,0 +1,4 @@
+package com.example.scafolmobile.fragment;
+
+public class FragmentProgress {
+}

3acfe4ab37de52e88e58ce6102251fd8e36feb8a
M. Ridwan Zalbina <zalbinaridwan@gmail.com>
2019-07-29 11:41:53 +0700

    Add Fragment Edit Location

--- ScafolMobile/app/src/main/java/com/example/scafolmobile/fragment/FragmentEditLokasi.java
+++ ScafolMobile/app/src/main/java/com/example/scafolmobile/fragment/FragmentEditLokasi.java
@@ -1,11 +1,15 @@
 package com.example.scafolmobile.fragment;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -14,16 +18,51 @@ import androidx.fragment.app.Fragment;
 
 import com.example.scafolmobile.R;
 
+import org.osmdroid.api.IMapController;
+import org.osmdroid.config.Configuration;
+import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
+import org.osmdroid.util.GeoPoint;
+import org.osmdroid.views.MapView;
+import org.osmdroid.views.overlay.Marker;
+
 public class FragmentEditLokasi extends Fragment {
     public static String TAG = "FragmentEditLokasi";
+    MapView map = null;
+    Context context;
+    GeoPoint startPoint;
+    Marker startMarker;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_uploaddata, container, false);
-        Intent intent = getActivity().getIntent();
-        String id_paket = intent.getStringExtra("pa_id");
-        Log.d(TAG, "PA_ID In fragment edit lokasi");
+        LinearLayout view = (LinearLayout) inflater.inflate(R.layout.fragment_editlokasi, container, false);
+        Context ctx = getActivity();
+        Configuration.getInstance().load(ctx, PreferenceManager.getDefaultSharedPreferences(ctx));
+        map = (MapView) view.findViewById(R.id.map2);
+        map.setTileSource(TileSourceFactory.MAPNIK);
+        map.setBuiltInZoomControls(true);
+        map.setMultiTouchControls(true);
+        IMapController mapController = map.getController();
+        mapController.setZoom(11);
+        startPoint = new GeoPoint(-2.9547949, 104.6929245);
+        mapController.setCenter(startPoint);
         return view;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        map.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        map.onResume();
     }
 }

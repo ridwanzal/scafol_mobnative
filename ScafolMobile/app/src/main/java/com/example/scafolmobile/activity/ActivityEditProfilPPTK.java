ee082e61b7cb4034bc74c35244bb37c0662f45eb
M. Ridwan Zalbina <zalbinaridwan@gmail.com>
2019-07-30 14:26:29 +0700

    Add Profile Info

--- ScafolMobile/app/src/main/java/com/example/scafolmobile/activity/ActivityEditProfilPPTK.java
+++ ScafolMobile/app/src/main/java/com/example/scafolmobile/activity/ActivityEditProfilPPTK.java
@@ -1,7 +1,10 @@
 package com.example.scafolmobile.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -12,5 +15,18 @@ public class ActivityEditProfilPPTK extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_editprofilepptk);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        Intent intent_get;
+        String get_value = getIntent().getDataString();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case android.R.id.home :
+                finish();
+            default :
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
